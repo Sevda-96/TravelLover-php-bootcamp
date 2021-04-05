06ed9fc3e368fb38767f8695e6fe4700b10f88aa
Sevda Engin <53494300+Sevda-96@users.noreply.github.com>
2021-04-05 22:18:55 +0300

    Update delete_tour.php

--- admin/delete_tour.php
+++ admin/delete_tour.php
@@ -5,8 +5,6 @@ include"layouts/db.php";
 <?php 
 $id=$_GET['id'];
 
-   // $dlt=$conn->prepare("DELETE FROM tours WHERE tours_id=?");
-    //$dlt->execute([$id]);
     $is_deleted=true;
     $update=$conn->prepare("UPDATE tours SET is_deleted=:is_deleted WHERE tours_id=:id ");
         $update->execute([
