db4e68f3de671256aaa701de35b42ba21dc55a9f
Sevda Engin <53494300+Sevda-96@users.noreply.github.com>
2021-04-05 22:18:31 +0300

    Update delete_reservation.php

--- admin/delete_reservation.php
+++ admin/delete_reservation.php
@@ -5,8 +5,6 @@ include"layouts/db.php";
 <?php 
 $id=$_GET['id'];
 
-   // $dlt=$conn->prepare("DELETE FROM tours WHERE tours_id=?");
-    //$dlt->execute([$id]);
     $is_deleted=true;
     $update=$conn->prepare("UPDATE reservation SET is_deleted=:is_deleted WHERE reservation_id=:id ");
         $update->execute([
